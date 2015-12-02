f555b3276a61dfc176a2e3dd9710c2880e7b0d88
VeskoMitev <vesko.mitev@gmail.com>
2015-12-02 09:32:40 +0200

    Homework

--- Problem_01_OddOrEvenIntegers/OddOrEvenIntegers.cs
+++ Problem_01_OddOrEvenIntegers/OddOrEvenIntegers.cs
@@ -5,6 +5,18 @@
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter number:");
+            int inputNumber = int.Parse(Console.ReadLine());
+            
+            Console.Write("This number is ");
+            if (inputNumber%2 == 0)
+            {
+                Console.WriteLine("even");
+            }
+            else
+            {
+                Console.WriteLine("odd");
+            }
         }
     }
 }
