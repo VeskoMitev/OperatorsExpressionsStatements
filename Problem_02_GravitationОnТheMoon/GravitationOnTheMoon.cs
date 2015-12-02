4aebf833c22a95c7ffd128559da7103bc94bc3f8
VeskoMitev <vesko.mitev@gmail.com>
2015-12-02 09:50:24 +0200

    Homework

---
+++ Problem_02_GravitationОnТheMoon/GravitationOnTheMoon.cs
@@ -0,0 +1,15 @@
+﻿namespace OperatorsExpressionsStatements
+{
+    using System;
+
+    class GravitationOnTheMoon
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your weight:");
+            double weight = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Your weight on the moon: {0} kg",weight * 0.17);
+        }
+    }
+}
