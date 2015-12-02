b130ca798fde4c02b2cb27ae94ae3d5f8ca5dded
VeskoMitev <vesko.mitev@gmail.com>
2015-12-02 10:09:48 +0200

    Homework

---
+++ Problem_03_DivideBy_7_and_5/DivideBy_7_and_5.cs
@@ -0,0 +1,14 @@
+﻿namespace OperatorsExpressionsStatements
+{
+    using System;
+    class DivideBy_7_and_5
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter integer number:");
+            int inputNumber = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Divide by 7 and 5: {0}", (inputNumber%7 == 0 && inputNumber%5 == 0));
+        }
+    }
+}
---
+++ Problem_04_Rectangles/Rectangles.cs
@@ -0,0 +1,18 @@
+﻿namespace OperatorsExpressionsStatements
+{
+    using System;
+    class Rectangles
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Rectangle width: ");
+            double width = double.Parse(Console.ReadLine());
+            Console.Write("Rectangle heigh: ");
+            double heigh = Double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Perimeter: {0}",(2*width)+(2*heigh));
+            Console.WriteLine("Area: {0} ",width* heigh );
+
+        }
+    }
+}
